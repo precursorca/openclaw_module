32f3984cc2438025ca08e41e80735e49192578a1
Alex Narvey <anarvey@precursor.ca>
2026-03-11 08:03:08 -0500

    Add Openclaw_processor class for data processing

---
+++ openclaw_processor.php
@@ -0,0 +1,26 @@
+<?php
+
+use munkireport\processors\Processor;
+
+class Openclaw_processor extends Processor
+{
+    public function run($data)
+    {
+        $modelData = ['serial_number' => $this->serial_number];
+
+		// Parse data
+        $sep = ' = ';
+		foreach(explode("\n", $data) as $line) {
+            if($line){
+                list($key, $val) = explode($sep, $line);
+                $modelData[$key] = $val;
+            }
+		} //end foreach explode lines
+
+        Openclaw_model::updateOrCreate(
+            ['serial_number' => $this->serial_number], $modelData
+        );
+        
+        return $this;
+    }   
+}
